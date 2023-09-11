59ff5ff00c004ecd9a4ca3cda20c8ba3852d2d08
sabarigrisan <112703518+sabarigrisan@users.noreply.github.com>
2023-09-11 11:42:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; 
 
-var randomImageSource = "/" + randomDiceImage; 
-
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
